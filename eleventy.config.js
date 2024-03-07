e31eaa8befaa6d8bfed4ba3b99475005d6505d92
Michel Maillard <info@maillard.li>
2024-03-07 14:01:49 +0100

    Corrections + data cascade

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/admin");
+	eleventyConfig.addGlobalData('dataCascade.id', 'config global data, 2nd in the cascade.');
 return {
   dir: {
     input: "src",
---
+++ src/elements/elements.11tydata.js
@@ -0,0 +1,7 @@
+module.exports = {
+	eleventyComputed: {
+		dataCascade: {
+			id: "computed data, highest priority.",
+		}
+	}
+};
